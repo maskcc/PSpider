882fec4dfdc6f2511204850f1a93d15be38b4bcb
pickmio <394283224@qq.com>
2015-12-15 14:25:45 +0800

    how to process redirect

--- login.py
+++ login.py
@@ -2,11 +2,8 @@
 import urllib.request
 import urllib.parse
 from urllib.error import HTTPError, URLError
-
+import http.cookiejar
 LoginPage = "https://www.secure.pixiv.net/login.php"
-
-
-
 
 header_dic = {
         "ContentType" : "application/x-www-form-urlencoded",\
@@ -14,7 +11,7 @@ header_dic = {
         "Referer" : "https://www.secure.pixiv.net/login.php",\
         "Accept" : "test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",\
         "Accept-Language" : "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",\
-        "Accept-Encoding" : "gzip, deflate",\
+       # "Accept-Encoding" : "gzip, deflate",\
         "Connection" : "keep-alive",\
         "Content-Length" : 47,\
         "Cache-Control" : "max-age=0",\
@@ -31,16 +28,39 @@ post_data = {
 DATA = urllib.parse.urlencode(post_data)
 DATA = DATA.encode(encoding='utf-8')
 
-url = urllib.request.Request(url=LoginPage,data=DATA,headers=header_dic,method='POST')
+class HTTPRedirectHandler(urllib.request.HTTPRedirectHandler):
+    def http_error_302(self, req, fp, code, msg, headers):
+        print("Redirect is called but I get it")
 
+
+url = urllib.request.Request(url=LoginPage,data=DATA,headers=header_dic,method='POST')
+'''
 try :
         response = urllib.request.urlopen(url)
 except HTTPError as e:
         print(e.code, e.reason)
-web = response.read()
+web = response.readline()
 print(response.code)
 print(response.reason)
-f = open("ss.txt", 'wb+')
-f.write(web)
+print(web.decode('utf8'))
+#html = open("pixiv.html", 'wb+')
+#html.write(web)
+print(response.info())
+'''
 
-
+redHandler = urllib.request.HTTPRedirectHandler()
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj) , HTTPRedirectHandler())
+try:
+    web = opener.open(url)
+except HTTPError as e:
+    print(e)
+    
+print("headers:")
+print("return code is", web.code)
+print(web.info())
+print(web.geturl())
+print("cookie:")
+cook = cj.make_cookies(web, url)
+for item in cook:
+    print(item)
