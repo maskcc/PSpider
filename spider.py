d35745b34d0951c312367be8dc4d0ddf77da62e9
mio <pickmio@163.com>
2015-12-22 23:09:05 +0800

    replace the \\r\\n to a blank , for fear that some lines can not match ,I would  better change the register to lxml.lib or what

--- spider.py
+++ spider.py
@@ -52,15 +52,24 @@ rsp = urllib.request.urlopen(request)
 print(rsp.code)
 htmlPage = rsp.read()
 htmlPage = htmlPage.decode('utf-8')
+htmlPage = htmlPage.replace('\r\n', ' ')
 with open('htmlPage.html', 'w') as f:
     f.write(htmlPage)
 #pattern = re.compile(r'(?<=<ul class="user-list"><li><a href="/member.php\?id=)\d+')
 pattern = re.compile(r'<div class="userdata"><a href="member.php\?id=\d+" class="ui-profile-popup" data-user_id="\d+" data-profile_img=.*?(?=<br><span>)')
 match = pattern.findall(htmlPage)
 users = []
+i = 0
+print(len(match))
 if match:
     for keys in match:
         users.append(parseUser(keys))
 
 for v in users:
-    v.show()
+    request = urllib.request.Request(v.imgSrc, headers = header)
+    request.add_header('Cookie', Login.cookie)
+    rsp = urllib.request.urlopen(request)
+    img = rsp.read()
+    imgfile = open(r'./file/' + v.ID + '.' + v.imgSrc.split('.')[-1], 'wb+')
+    imgfile.write(img)
+
